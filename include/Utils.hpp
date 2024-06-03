af4f70b5b8e9f0b246321a81e8f36fe4a126a2f7
nerdaxe <github@nerdaxe.org>
2024-06-03 21:01:22 +0200

    Update

---
+++ include/Utils.hpp
@@ -0,0 +1,23 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+#include <type_traits>
+#include <etl/vector.h>
+
+
+namespace libutils {
+
+	bool ToLittleEndian(void* buffer, uint16_t length);
+	bool ToLittleEndian(const void* buffer, uint16_t length, void *output, uint16_t max_length);
+
+	bool HexdecimalToBinary(char *str, uint16_t length);
+	bool HexdecimalToBinary(const char* str, uint16_t length, uint8_t*output, uint16_t max_length);
+
+	bool BinaryToHexdecimal(uint8_t *output, uint16_t length, const uint8_t* input, uint16_t input_length);
+	bool BinaryToHexdecimal(uint8_t* output, uint16_t length, const etl::ivector<uint8_t>& vec);
+
+
+	bool HexdecimalToBinary(const char* str, uint16_t length, etl::ivector<uint8_t>& vec);
+
+}
+
+#endif // !UTILS_HPP
