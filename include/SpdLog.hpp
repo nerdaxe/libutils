7572edde198ddbfae87e1c548639044fcb7e9471
nerdaxe <github@nerdaxe.org>
2024-06-05 17:59:35 +0200

    Update

--- include/SpdLog.hpp
+++ include/SpdLog.hpp
@@ -4,11 +4,12 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/ostr.h>
 
-std::ostream& operator<<(std::ostream& os, const etl::string_view& c)
+static std::ostream& operator<<(std::ostream& os, const etl::string_view& c)
 {
 	std::string str(c.data(), c.length());
 	return os << str;
 }
+
 template <> struct fmt::formatter<etl::string_view> : fmt::ostream_formatter {};
 
 #endif
